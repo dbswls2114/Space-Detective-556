2d90a811d3584e0d6b4abe2758583a26db0cf043
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2023-12-01 21:01:44 +0900

    fix

--- Assets/Scripts/Contollers/PlayerInputContoller.cs
+++ Assets/Scripts/Contollers/PlayerInputContoller.cs
@@ -5,15 +5,12 @@ using UnityEngine.InputSystem;
 
 
 public class PlayerInputContoller : PlayerCharacterContoller
-
 {
     private Camera _camera;
     Animator anim;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
-    
-
-    
+      
     private void Awake()
     {
         _camera = Camera.main;
@@ -22,17 +19,13 @@ public class PlayerInputContoller : PlayerCharacterContoller
         rigid.freezeRotation = true;
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();  
-    }   
-      
-
-
     }
+
     public void OnMove(InputValue value)
     {
         Vector2 moveInput = value.Get<Vector2>().normalized;
         CallMoveEvent(moveInput);
     }
-
     public void OnAttack(InputValue value)
     {
         IsAttacking = value.isPressed;
