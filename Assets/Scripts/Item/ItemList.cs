d0f9fb90b477f4b4205d5f17e999d8d3bae5653f
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2023-12-05 20:01:05 +0900

    Item Move add
    아이템 움직임 추가

--- Assets/Scripts/Item/ItemList.cs
+++ Assets/Scripts/Item/ItemList.cs
@@ -17,31 +17,34 @@ public enum ItemType
     public float maxSizeY = 5f; // 카메라 범위
     public Rigidbody2D _rigidbody;
 
-    public float speed = 10f; //임시
+    public float speed = 1f; //임시
     public abstract void ItemEffect(); //안씀
     public abstract void SpawnItem(); //안씀
+
+    public Vector2 move = new Vector2(1, 1);
 
     protected virtual void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        
     }
 
     protected void FixedUpdate()
     {
-        //Move();
+        Move();
     }
 
-    /* 아이템이 드랍되면 움직이게 하는부분
+    
     protected void Move()
     {
-        _rigidbody.AddForce(transform.forward * -speed, ForceMode2D.Force);
-        if(this.transform.position.y <  -maxSizeY ) //일단 카메라 범위를 벗어나면 강제로 돌아가게 하는데 다른 이동방식을 생각 해 보기
+        _rigidbody.velocity = move*speed;
+        if (this.transform.position.y <  -maxSizeY || this.transform.position.y >= maxSizeY) //일단 카메라 범위를 벗어나면 강제로 돌아가게 하는데 다른 이동방식을 생각 해 보기
         {
-            
-        }else if(this.transform.position.x > maxSizeX || this.transform.position.x < -maxSizeX)
-        {
-
+            move = new Vector2(this.transform.position.x,this.transform.position.y*-1).normalized;
         }
-    }
-    */
+        if(this.transform.position.x >= maxSizeX || this.transform.position.x < -maxSizeX)
+        {
+            move = new Vector2(this.transform.position.x * -1,this.transform.position.y).normalized;
+        }
+    }   
 }
